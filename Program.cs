1bac686b01548bd7d44d312bc189b6bf6293b084
Dante Cifuentes <dante.cifuentes@yahoo.com>
2016-07-26 17:19:34 -0400

    adding comments
    The first version has no comments.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,10 @@ namespace OneAndZeros
     {
         static void Main(string[] args)
         {
+
+            //const, it could be replaced by an user input
             const int number = 9;
+            //local variables
             var i = 1;
             int multiple;
             bool isZerosAndOnes;
@@ -30,14 +33,15 @@ namespace OneAndZeros
                 {
                     if ((!array[j].Equals('1') && !array[j].Equals('0')))
                     {
+                        //if the array contains at least one ocurrence of digits different from 1 or 0, we stopped the search
                         isZerosAndOnes = false;
                     }
-                    j++;
+                    j++; //next element in the char array
                 } while (j < array.Length && isZerosAndOnes);
 
             } while (!isZerosAndOnes);
 
-
+            //output
             Console.WriteLine("The number {0}, is {1} times {2}, and it only contains 1 & 0s", multiple, i, number);
             Console.ReadLine();
 
