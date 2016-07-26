a0fa001a78f4d1a2baaa7c47cd35d0177c8d01fc
Dante Cifuentes <dante.cifuentes@yahoo.com>
2016-07-26 17:14:37 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneAndZeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int number = 9;
+            var i = 1;
+            int multiple;
+            bool isZerosAndOnes;
+            char[] array;
+
+            do
+            {
+                i++;
+                multiple = number*i;
+                isZerosAndOnes = true;
+
+                array = multiple.ToString(CultureInfo.InvariantCulture).ToCharArray();
+
+                int j = 0;
+                do
+                {
+                    if ((!array[j].Equals('1') && !array[j].Equals('0')))
+                    {
+                        isZerosAndOnes = false;
+                    }
+                    j++;
+                } while (j < array.Length && isZerosAndOnes);
+
+            } while (!isZerosAndOnes);
+
+
+            Console.WriteLine("The number {0}, is {1} times {2}, and it only contains 1 & 0s", multiple, i, number);
+            Console.ReadLine();
+
+        }
+    }
+}
